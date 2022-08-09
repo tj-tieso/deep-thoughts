bd52039eb5351f131b2276f8a8d2bd9828fd46c8
tj-tieso <tj.tieso@gmail.com>
2022-08-09 09:42:04 -0500

    addFriend Mutation

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -38,6 +38,7 @@ const typeDefs = gql`
   }
 
   type Query {
+    me: User
     users: [User]
     user(username: String!): User
     thoughts(username: String): [Thought]
@@ -47,6 +48,9 @@ const typeDefs = gql`
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
+    addThought(thoughtText: String!): Thought
+    addReaction(thoughtId: ID!, reactionBody: String!): Thought
+    addFriend(friendId: ID!): User
   }
 `;
 
