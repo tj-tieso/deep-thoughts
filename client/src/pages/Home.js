b76465983b8f36a11402f2781abc7d10116f86a2
tj-tieso <tj.tieso@gmail.com>
2022-08-10 13:21:55 -0500

    login

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,21 +1,28 @@
 import React from "react";
 import { useQuery } from "@apollo/client"; //Hook from Apollo Client to make requests to the GraphQL server
-import { QUERY_THOUGHTS } from "../utils/queries";
+import { QUERY_THOUGHTS, QUERY_ME_BASIC } from "../utils/queries";
+import Auth from "../utils/auth";
 import ThoughtList from "../components/ThoughtList";
+import FriendList from "../components/FriendList";
 
 const Home = () => {
   // use useQuery hook to make query request
   const { loading, data } = useQuery(QUERY_THOUGHTS);
 
+  // use object destructuring to extract `data` from the `useQuery` Hook's response
+  // and rename it `userData` to be more descriptive
+  const { data: userData } = useQuery(QUERY_ME_BASIC);
+
   // if data exists, store it in the thoughts constant we just created.
   // If data is undefined, then save an empty array to the thoughts component.
   const thoughts = data?.thoughts || [];
-  console.log(thoughts);
+
+  const loggedIn = Auth.loggedIn();
 
   return (
     <main>
       <div className="flex-row justify-space-between">
-        <div className="col-12 mb-3">
+        <div className={`col-12 mb-3 ${loggedIn && "col-lg-8"}`}>
           {loading ? (
             <div>Loading...</div>
           ) : (
@@ -24,6 +31,15 @@ const Home = () => {
               title="Some Feed for Thought(s)..."
             />
           )}
+          {loggedIn && userData ? (
+            <div className="col-12 col-lg-3 mb-3">
+              <FriendList
+                username={userData.me.username}
+                friendCount={userData.me.friendCount}
+                friends={userData.me.friends}
+              />
+            </div>
+          ) : null}
         </div>
       </div>
     </main>
